e290fbf8000304ddc7e41a71c431f7cccb5d3940
Sergey Nagornykh <serega1905@ukr.net>
2024-06-06 17:41:38 +0300

    :wrench: config(vite): add mixins.scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/mixins.scss";
+        `,
+      },
+    },
+  },
 })
